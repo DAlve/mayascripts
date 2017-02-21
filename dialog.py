98b9fa68ed546b08b50b8c323ba2cc032d2492bb
DAlve <dave.alve@gmail.com>
2017-02-21 21:43:24 +1300

    Updated files that are now working

--- dialog.py
+++ dialog.py
@@ -4,40 +4,41 @@ Created on Feb 20, 2017
 @author: davea
 '''
 import sys
-from Qt.QtCore import * 
-from Qt.QtGui import *
-from Qt.QtWidgets import *
-from Qt import QtCore
-from shiboken2 import wrapInstance
+from Qt import QtCore, QtGui, QtWidgets
+from maya import OpenMayaUI as omui
+#from maya.app.general.mayaMixin import MayaQWidgetBaseMixin, MayaQWidgetDockableMixin
 
 import dialog_ui
 reload(dialog_ui)
 
-from maya import OpenMayaUI as omui
+
 
 try:
-    from PySide2.QtUiTools import QUiLoader
+    from shiboken2 import wrapInstance
 except:
-    from PySide.QtUiTools import QUiLoader
+    from shiboken import wrapInstance
     
     
-    
-def get_maya_window():
-    mayaMainWindowPtr = omui.MQtUtil.mainWindow()
-    mayaMainWindow = wrapInstance(long(mayaMainWindowPtr), QWidget)
-    
-    return mayaMainWindow
 
-class dialog(QWidget):
+def get_maya_window():
+    ptr = omui.MQtUtil.mainWindow()
+    if ptr is not None:
+        return wrapInstance(long(ptr), QtWidgets.QWidget)
+
+class dialog(QtWidgets.QWidget):
     
     def __init__(self):
         super(dialog, self).__init__()
         
+        
+        self.ui = dialog_ui.Ui_Form()
+        self.ui.setupUi(self)
+        
         # Parent widget under Maya main Window
         self.setParent(get_maya_window())
-        self.setWindowFlags(Qt.Window)
+        self.setWindowFlags(QtCore.Qt.Window)
         
-        self.setAttribute(Qt.WA_DeleteOnClose, True)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose, True)
         
         # set the object name
         self.setObjectName('Dialog_uniqueId')
@@ -45,13 +46,10 @@ class dialog(QWidget):
         #self.setGeometry(50,50,250,150)
         self.setup_ui()
         
+        
     
     def setup_ui(self):
-        
         print 'We got here!'
-        self.ui = dialog_ui.Ui_Dialog()
-        #print test.__file__
-        self.ui.setupUi(self)
         
 
 def main():
@@ -59,5 +57,5 @@ def main():
     ui.show()
     return ui
 
-print __file__
+#print __file__
 main()
